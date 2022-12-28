967cee3deeda31bf72a8a1d8edbf523cbd08e6c4
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 21:44:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//name
+let userName = prompt('What is your name?', '')
+alert(`Your name is ${userName}`)
+
+// //year
+// const currentYear = 2022
+// let yourBirthYear = prompt('Your year of birth?', '')
+// let userAge = currentYear - yourBirthYear
+// alert(`You are ${userAge} years old`)
+
+// //square
+// let sideLength = prompt('The length of the side of the square?', '')
+// let squarePerimeter = sideLength * 4
+// alert(`The perimeter of the square is ${squarePerimeter} centimeters`)
+
+// //circle
+// let circleRadius = prompt('The radius of the circle?', '')
+// let circleSquare = Math.round(Math.PI * circleRadius ** 2)
+// alert(`The square of the circle is ${circleSquare}`)
+
+// //speed
+// let distance = prompt('Distance between two cities (in km)?', '')
+// let time = prompt('Duration of travel (in hours)?', '')
+// let speed = Math.round(distance / time)
+// alert(`Your speed must be ${speed} km/h`)
+
+// //convertor
+// alert('Convert USD to EUR')
+// let USD = prompt('Amount of USD', '')
+// let exchangeRate = 0.94
+// const EURO = USD * exchangeRate
+// alert(`${USD} USD is ${EURO} EURO`)
+
