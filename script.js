70ebeb7173283ff868b6f29788df27d6e6c8670a
Oleksii <oleksiizakomornyi@gmail.com>
2022-12-28 21:44:20 +0200

    add year

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let userName = prompt('What is your name?', '')
 alert(`Your name is ${userName}`)
 
-// //year
-// const currentYear = 2022
-// let yourBirthYear = prompt('Your year of birth?', '')
-// let userAge = currentYear - yourBirthYear
-// alert(`You are ${userAge} years old`)
+//year
+const currentYear = 2022
+let yourBirthYear = prompt('Your year of birth?', '')
+let userAge = currentYear - yourBirthYear
+alert(`You are ${userAge} years old`)
 
 // //square
 // let sideLength = prompt('The length of the side of the square?', '')
